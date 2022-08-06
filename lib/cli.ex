4584e93f01ea171d4d5714dad790df8109eb1a7a
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2022-08-05 19:58:11 -0700

    Add CLI as escript

---
+++ lib/cli.ex
@@ -0,0 +1,9 @@
+defmodule ExRPG.CLI do
+  @moduledoc """
+  The CLI for the project
+  """
+
+  def main(_args) do
+    IO.puts("Hello from ExRPG!")
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,8 +7,13 @@ defmodule ExRPG.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript(),
     ]
+  end
+
+  def escript do
+    [main_module: ExRPG.CLI]
   end
 
   # Run "mix help compile.app" to learn about applications.
