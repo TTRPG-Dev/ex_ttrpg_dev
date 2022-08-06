5ca9934d0a32d1700316f5d5094ac1788e758c97
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2022-08-05 20:00:19 -0700

    Add optimus as a dependency
    Optimus is a CLI argument parser package. We'll be using it for
    parsing arguments for the ex_rpg CLI.

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule ExRPG.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:optimus, "~> 0.3"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
