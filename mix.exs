d1437e021285a7e5d5a6f363eac845b2d74c1425
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2022-08-15 21:20:13 -0700

    Version 0.2.0 -> 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExRPG.MixProject do
   def project do
     [
       app: :ex_rpg,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
