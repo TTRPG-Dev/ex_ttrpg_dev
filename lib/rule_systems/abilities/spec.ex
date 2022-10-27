01982a9cb3b75b0bf6716bfb5d6f6385407a553c
Quigley Malcolm <tqmalcolm@protonmail.com>
2022-10-26 23:05:08 -0700

    Add helper methods for getting ability specs by name

--- lib/rule_systems/abilities/spec.ex
+++ lib/rule_systems/abilities/spec.ex
@@ -20,4 +20,18 @@ defmodule ExTTRPGDev.RuleSystems.Abilities.Spec do
   def get_names([%Spec{} | _tail] = specs) do
     Enum.reduce(specs, [], fn spec, acc -> [spec.name | acc] end)
   end
+
+  @doc """
+  Returns the spec for the given spec name
+
+  ## Examples
+
+      iex> ExTTRPGDev.RuleSystems.Abilities.Spec.get_spec_by_name(list_of_specs, spec_name)
+      %Spec{}
+  """
+  def get_spec_by_name([%Spec{} | _tail] = specs, spec_name) when is_bitstring(spec_name) do
+    Enum.find(specs, fn %Spec{name: name} ->
+      name == spec_name
+    end)
+  end
 end
